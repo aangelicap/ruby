4fcfdc66ca8e0d6be42efdce41eea23018e99869
Angelica Pelizza <aangelicap@users.noreply.github.com>
2020-07-24 13:48:28 -0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+
+Crie um software que permita que o usuário entre com um nome de um pokemon e receba como resposta sua lista de habilidades.
+
+Critérios:
+* Você deve utilizar orientação a objetos no seu software
+* O resultado de cada consulta do usuário deve ser guardado em um arquivo que possua no nome o pokemon, a data e horário da tradução (ex: bulbasaur-10-10-18_10:30.txt)
+
+Usar: https://pokeapi.co/
+
---
+++ poke.rb
@@ -0,0 +1,37 @@
+require 'rest-client'
+require 'json'
+require 'net/http'
+
+class Pokemon_Abilities
+    def initialize 
+        @name 
+        @url = 'https://pokeapi.co/api/v2/pokemon/'
+    end
+
+    def extract_abilities   
+        puts "Pokemons: venusaur - caterpie - rattata - ditto - kakuna"       
+        print "nome do pokemon: "
+        @name = gets.chomp
+
+        resp = RestClient.get "#{@url}/#{@name}"
+        @response = JSON.parse(resp.body)["abilities"].to_json
+
+        end
+
+    def show_abilities
+        data = @name + '-'+ Time.new.strftime('%d-%m-%y %H:%M') +'.txt'
+        file = File.open(data,'a')do |f|
+        @response_names = Array.new 
+        @response_names= @response["name"]
+        f.puts "Habilidades do #{@name}: #{@response_names}"    
+            f.puts "Json de habilidades do #{@name}: #{@response}"  
+        end
+    end
+
+end
+
+
+ poke = Pokemon_Abilities.new
+ poke.extract_abilities
+ poke.show_abilities
+
